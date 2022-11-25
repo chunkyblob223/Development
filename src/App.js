fcbdcdf40ca507db14103413f49038ff9c5126bf
chunkyblob223 <114885719+chunkyblob223@users.noreply.github.com>
2022-11-24 20:21:10 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
+import logo from './devlogo.png';
 import './App.css';
+import { useState } from "react";
+import {hatData} from "./hat-data";
 
 function App() {
+  const [total, setTotal] = useState(0);
+  const [type, setType] = useState("All");
+
+  function clickHandler(price, i_d){
+    if (document.getElementById(i_d).value == "Add to Cart"){
+      document.getElementById(i_d).value = "Remove From Cart";
+      // setTotal(total + price);
+    }
+    else {
+      document.getElementById(i_d).value = "Add to Cart";
+      // setTotal(total - price);
+    }
+  }
+
+
+  function selectFilterType(idk){
+    // sets the state to the selected type, function passed to the onSelect event handler of HTML buttons
+  }
+
+  function matchesFilterType(idk){
+    // creates a filtering condition
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" style={{ width: 300, height: 200 }} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hat Trick Hat Shop
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div class = "main-grid">
+        <div class = "side-bar">
+          <fieldset class = "Control-root">
+            <legend style={{ fontSize:30 }}> Sort By</legend>
+            <div class= "Form-group">
+              <div>
+                <div class = "label-flex"> <input type="checkbox" />
+                Popular </div>
+              </div>
+            </div>
+          </fieldset>
+          <div class = "style-filter">
+            <fieldset class = "Control-root">
+              <legend style={{ fontSize:30 }}>Style</legend>
+              <div class = "Form-group">
+                <div class = "label-flex"> <input type="checkbox" />
+                Baseball </div>
+                <div class = "label-flex"> <input type="checkbox" /> 
+                Cowboy</div>
+                <div class = "label-flex"> <input type="checkbox" /> 
+                Fedora</div>
+              </div>
+            </fieldset>
+          </div>
+          <div class = "material-filter">
+            <fieldset class = "Control-root">
+              <legend style={{ fontSize:30 }}>Material</legend>
+              <div class = "Form-group">
+                <div class = "label-flex"><input type="checkbox" /> 
+                Cloth</div>
+                <div class = "label-flex"> <input type="checkbox" />
+                Leather</div>
+                <div class = "label-flex"> <input type="checkbox" /> 
+                Straw</div>
+              </div>
+            </fieldset>
+          </div>
+          <div class = "cart-div">
+            <fieldset class = "Control-root">
+              <legend style={{ fontSize:30 }}>Cart</legend>
+                <div class = "Form-group">
+                  <label class = "label-flex"> <input type="checkbox" /> 
+                  My Cart</label>
+                </div>
+            </fieldset>
+          </div>
+          <h1 style = {{
+            marginTop: '-1rem', 
+            marginBottom:'1rem', 
+            color: 'grey', 
+            fontSize:'1.0rem'}}>
+            Cart Total: ${total}.00
+          </h1>
+        </div>
+        <div class = "item-grid">
+          {hatData.map((item) => {
+          return (
+            <div class = "per-item">
+              <div>
+              <p class = "color">
+                <h2>{item.name}</h2>
+                <p><i>Price:</i> ${item.price}.00</p>
+                <p><i>Style:</i> {item.style}</p>
+                <p><i>Material:</i> {item.material}</p>
+                <input class = "button" type = "button" id = {item.button_id} value = "Add to Cart" onClick = {() => clickHandler(item.price, item.button_id)}></input>
+              </p>
+              </div>
+              <div>
+                <img class = "App-logo" src = {item.image}></img>
+              </div>
+            </div>
+          );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/hat-data.js
@@ -0,0 +1,98 @@
+export const hatData = [
+    {
+        "name": "Sailing Baseball Cap",
+        "price": 15.00,
+        "style": "Baseball",
+        "material": "Cloth",
+        "image": require("./images/baseball_cloth2.png"),
+        "button_id": "first_id"
+    },
+    {
+        "name": "Radically Simple Cowboy",
+        "price": 32.00,
+        "style": "Cowboy",
+        "material": "Cloth",
+        "image": require("./images/cloth_cowboy.png"),
+        "button_id": "second_id"
+    },
+    {
+        "name": "Cool Dudes Rule Cap",
+        "price": 25.00,
+        "style": "Baseball",
+        "material": "Cloth",
+        "image": require("./images/cloth_baseball.png"),
+        "button_id": "third_id"
+    },
+    {
+        "name": "Mountainous Cap",
+        "price": 30.00,
+        "style": "Baseball",
+        "material": "Cloth",
+        "image": require("./images/cloth_baseball2.png"),
+        "button_id": "fourth_id"
+    },
+    {
+        "name": "Soft Pink Fedora",
+        "price": 55.00,
+        "style": "Fedora",
+        "material": "Cloth",
+        "image": require("./images/cloth_fedora2.png"),
+        "button_id": "fifth_id"
+    },
+    {
+        "name": "Leather Baseball Cap",
+        "price": 17.00,
+        "style": "Baseball",
+        "material": "Leather",
+        "image": require("./images/leather_bs.png"),
+        "button_id": "sixth_id"
+    },
+    {
+        "name": "Slim Fedora",
+        "price": 60.00,
+        "style": "Fedora",
+        "material": "Cloth",
+        "image": require("./images/cloth_fedora.png"),
+        "button_id": "seventh_id"
+    },
+    {
+        "name": "Leather Cowboy",
+        "price": 100.00,
+        "style": "Cowboy",
+        "material": "Leather",
+        "image": require("./images/leather_cowbody.png"),
+        "button_id": "eight_id"
+    },
+    {
+        "name": "Basic Straw Baseball Cap",
+        "price": 10.00,
+        "style": "Baseball",
+        "material": "Straw",
+        "image": require("./images/straw_baseball.png"),
+        "button_id": "ninth_id"
+    },
+    {
+        "name": "Straw Cowboy Hat",
+        "price": 35.00,
+        "style": "Cowboy",
+        "material": "Straw",
+        "image": require("./images/straw_cowboy.png"),
+        "button_id": "tenth_id"
+    },
+    {
+        "name": "Classic Fedora",
+        "price": 43.00,
+        "style": "Fedora",
+        "material": "Straw",
+        "image": require("./images/straw_fedora.png"),
+        "button_id": "eleventh_id"
+    },
+    {
+        "name": "Silly Crazy Pink Cowboy Hat",
+        "price": 29.00,
+        "style": "Cowboy",
+        "material": "Straw",
+        "image": require("./images/straw2_cowboy.png"),
+        "button_id": "twelve_id"
+    }
+]
